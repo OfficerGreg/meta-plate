b92f03c097d8ea673464080c0edbd48279a354b3
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-06-03 20:03:58 +0200

    Added notes

--- app.py
+++ app.py
@@ -5,17 +5,17 @@ from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
 from flask_bcrypt import Bcrypt
 
-
 from models import db, User
 from register import RegisterForm
 from login import LoginForm
-from flask_bcrypt import Bcrypt
+from apikey import ApiKeyForm
+
 
 app = Flask(__name__)
 
 bcrypt = Bcrypt(app)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db/database.db"
 app.config["SECRET_KEY"] = "secret_key"
 
 login_manager = LoginManager()
@@ -34,12 +34,13 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
+#Routes
 
 @app.route("/")
 def home():
    return render_template("home.html")
 
-@app.route("/login", methods=['GET', 'POST'])
+@app.route("/login", methods=["GET", "POST"])
 def login():
    #show login form
    form = LoginForm()
@@ -57,7 +58,20 @@ def login():
 @app.route("/dashboard", methods=["GET", "POST"])
 @login_required
 def dashboard():
-   return render_template("dashboard.html")
+   user = current_user
+
+   form = ApiKeyForm()
+   if form.validate_on_submit():
+      # Get the logged-in user
+      user = current_user
+
+      # Update the user's API key
+      user.api_key = form.api_key.data
+      db.session.commit()
+      return redirect(url_for("dashboard"))
+
+   notes = user.get_notes()
+   return render_template("dashboard.html", form=form, notes=notes)
 
 @app.route("/logout", methods=["GET", "POST"])
 @login_required
@@ -75,9 +89,26 @@ def register():
       new_user = User(username=form.username.data, password=hashed_password)
       db.session.add(new_user)
       db.session.commit()
-      return redirect(url_for('login'))
+      return redirect(url_for("login"))
 
    return render_template("register.html", form=form)
 
-if __name__ == '__main__':
+@app.route("/notes", methods=["GET", "POST"])
+@login_required
+def notes():
+   # Get the logged-in user
+   user = current_user
+
+   # Add a new note
+   user.add_note("This is a new note")
+   db.session.commit()
+
+   # Retrieve all notes
+   notes = user.get_notes()
+
+   return render_template("notes.html", notes=notes)
+
+
+
+if __name__ == "__main__":
    app.run(debug=True)
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 
+import json
+
 db = SQLAlchemy()
 
 # Table Model for DB
@@ -8,3 +10,16 @@ class User(db.Model, UserMixin):
    id = db.Column(db.Integer, primary_key=True)
    username = db.Column(db.String(30), nullable=False, unique=True)
    password = db.Column(db.String(100), nullable=False)
+   api_key  = db.Column(db.String(100))
+   notes    = db.Column(db.String)
+
+   def set_notes(self, notes):
+      self.notes = json.dumps(notes)
+
+   def get_notes(self):
+      return json.loads(self.notes) if self.notes else []
+
+   def add_note(self, note):
+      notes = self.get_notes()
+      notes.append(note)
+      self.set_notes(notes)
