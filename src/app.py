1a950bf929d6f74e08507810c095e6fb4b14a029
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-06-05 17:51:54 +0200

    clean up

--- src/app.py
+++ src/app.py
@@ -14,8 +14,10 @@ from ai import AI
 
 import sys
 
+template_dir = "../templates/"
+static_dir = "../static/"
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder=template_dir, static_folder=static_dir)
 
 bcrypt = Bcrypt(app)
 
