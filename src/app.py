8417435e55766ccc9889308ccb43aeb3c03d5782
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-06-07 21:36:59 +0200

    added 404 page

--- src/app.py
+++ src/app.py
@@ -143,8 +143,9 @@ def delete_note():
 
 
 #Error handling
-
-
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('404.html'), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
