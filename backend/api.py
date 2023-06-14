260340b6f4d1fa32abec584b4b85a02c45bc59a0
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-06-14 21:46:26 +0200

    sse stream gpt prototype

--- backend/api.py
+++ backend/api.py
@@ -1,11 +1,11 @@
 from flask import Blueprint, Response
 import openai
-import time
-from datetime import datetime
 
 api_bp = Blueprint("api", __name__)
 
-API_KEY = 'hBah7VMdi_6TsDBGE4PeFwJauklPpPcB7bPS45UveNg'
+# remove # from API KEY
+
+API_KEY = '#hBah7VMdi_6TsDBGE4PeFwJauklPpPcB7bPS45UveNg'
 API_URL = 'https://chimeragpt.adventblocks.cc/v1'
 
 openai.api_key = API_KEY
@@ -15,8 +15,12 @@ openai.api_base = API_URL
 @api_bp.route('/stream')
 def stream():
     def get_data():
-        while True:
-            time.sleep(1)
-            yield f'data: {datetime.now()} \n\n'
-        
-    return Response(get_data(), mimetype="text/event-stream")
+        completion = openai.ChatCompletion.create(
+            model='gpt-3.5-turbo', 
+            messages=[{"role": "user", "content": "What is the difference between python and C++?"}],
+            stream=True)
+        for line in completion:
+            chunk = line['choices'][0].get('delta', {}).get('content')
+            if chunk:
+                yield f'data: {chunk}\n\n'
+    return Response(get_data(), mimetype='text/event-stream')
