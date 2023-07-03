d389458cb394528d29d44f1b33e61fb8139a2e92
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-07-03 17:26:49 +0200

    Added create quiz and question route

--- backend/quiz.py
+++ backend/quiz.py
@@ -1,25 +1,58 @@
 from flask import Blueprint, jsonify, session, request
 
-from models import User, Quiz, db
+from models import User, Quiz, Question, db
 
 quiz_bp = Blueprint("quiz", __name__)
 
 
 @quiz_bp.route("/quiz")
-def dashboard():
+def get_all_quizzes():
     user_id = session.get("user_id")
 
     if not user_id:
         return jsonify({"error": "unauthorized"}), 401
 
-    user = User.query.filter_by(id=user_id).first()
+    # Retrieve all quizzes ordered by creation time
+    quizzes = Quiz.query.order_by(Quiz.id).all()
 
-    return jsonify({
-        "id": user.id,
-        "username": user.username,
-        "email": user.email
-    })
+    # Prepare the response
+    quizzes_data = []
+    for quiz in quizzes:
+        quiz_data = {
+            "id": quiz.id,
+            "name": quiz.name,
+            "created_by": {
+                "id": quiz.user.id,
+                "username": quiz.user.username,
+                "email": quiz.user.email
+            },
+            "questions": []  # Add an empty list to store questions
+        }
 
+        # Get the questions for the quiz
+        questions = quiz.questions
+        for question in questions:
+            question_data = {
+                "id": question.id,
+                "text": question.text,
+                "answers": []  # Add an empty list to store answers
+            }
+
+            # Get the answers for the question
+            answers = question.answers
+            for answer in answers:
+                answer_data = {
+                    "id": answer.id,
+                    "text": answer.text,
+                    "is_correct": answer.is_correct
+                }
+                question_data["answers"].append(answer_data)
+
+            quiz_data["questions"].append(question_data)
+
+        quizzes_data.append(quiz_data)
+
+    return jsonify({"quizzes": quizzes_data})
 
 
 #create quiz route
@@ -44,3 +77,36 @@ def create_quiz():
     db.session.commit()
 
     return jsonify({"message": "Quiz created successfully"})
+
+
+#add question to quiz route
+@quiz_bp.route("/quiz/<int:quiz_id>/add-question", methods=["POST"])
+def add_question_to_quiz(quiz_id):
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({"error": "unauthorized"}), 401
+
+    user = User.query.get(user_id)
+    if not user:
+        return jsonify({"error": "user not found"}), 404
+    
+    quiz = Quiz.query.get(quiz_id)
+    if not quiz:
+        return jsonify({"error": "quiz not found"}), 404
+    
+    # check if the user is the creator of the quiz
+    if quiz.user_id != user.id:
+        return jsonify({"error": "forbidden"}), 403
+    
+    # Extract question details from the request
+    data = request.json
+    question_text = data.get("question")
+
+    # Create the question
+    question = Question(text=question_text, quiz=quiz)
+    db.session.add(question)
+    db.session.commit()
+
+    return jsonify({"message": "Question added successfully"})
+
