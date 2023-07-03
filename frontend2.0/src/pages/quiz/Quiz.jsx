a9e08e2c092fac20b4c099e4163db4226a0ead82
OfficerGreg <Jakub.Gancarz@protonmail.com>
2023-07-03 17:50:22 +0200

    Quiz Frontend

--- frontend2.0/src/pages/quiz/Quiz.jsx
+++ frontend2.0/src/pages/quiz/Quiz.jsx
@@ -1,68 +1,54 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, Link } from 'react-router-dom';
-
-import { User, Folder, Note } from '../../types';
 import httpClient from '../../httpClient';
-import StatusCard from '../../components/status-card/StatusCard'
-import statusCards from '../../assets/JsonData/status-card-data.json'
-
-
 
 const Quiz = () => {
-    const [user, setUser] = useState(null);
-    const history = useHistory();
+  const [quizzes, setQuizzes] = useState([]);
+  const history = useHistory();
 
-    const logoutUser = async () => {
-        await httpClient.post("//localhost:5000/logout");
-        history.push("/");
-    };
-
-
-    const fetchUserData = async () => {
-        try {
-            const response = await httpClient.get("//localhost:5000/@me");
-            const userData = response.data;
-            setUser(userData);
-
-        } catch (error) {
-            console.log("Not authenticated");
-            history.push("/login");
-        }
-    };
-
-    useEffect(() => {
-        fetchUserData();
-    }, []);
-
-
-    if (user === null) {
-        return <h1>Loading...</h1>;
+  const fetchQuizData = async () => {
+    try {
+      const response = await httpClient.get("//localhost:5000/quiz");
+      const quizData = response.data.quizzes;
+      setQuizzes(quizData);
+    } catch (error) {
+      console.log("Error fetching quiz data:", error);
     }
+  };
 
-    return (
-        <div>
-            <h2 className="page-header">Quiz</h2>
-            <div className="row">
-                <div className="col-6">
-                    <div className="row">
-                        {
-                            statusCards.map((item, index) => (
-                                <div className="col-6" key={index}>
+  useEffect(() => {
+    fetchQuizData();
+  }, []);
 
-                                    <StatusCard
-                                        icon={item.icon}
-                                        count={item.count}
-                                        title={item.title}
-
-                                    />
-                                </div>
-                            ))
-                        }
-                    </div>
+  return (
+    <div>
+      <h2 className="page-header">Quiz</h2>
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            {quizzes.map((quiz) => (
+              <div className="col-6" key={quiz.id}>
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">{quiz.name}</h5>
+                    <p className="card-text">
+                      Created by: {quiz.created_by.username}
+                    </p>
+                    <Link
+                      to={`/quiz/${quiz.id}`}
+                      className="btn btn-primary"
+                    >
+                      Start Quiz
+                    </Link>
+                  </div>
                 </div>
-            </div>
+              </div>
+            ))}
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Quiz;
--- frontend2.0/src/types.js
+++ frontend2.0/src/types.js
@@ -18,3 +18,24 @@ export const Note = {
   folder_id: 0,
   user_id: 0,
 };
+
+export const Quiz = {
+  id: 0,
+  name: "",
+  user_id: 0,
+  questions: [],
+};
+
+export const Question = {
+  id: 0,
+  question: "",
+  quiz_id: 0,
+  answers: [],
+};
+
+export const Answer = {
+  id: 0,
+  answer: "",
+  is_correct: false,
+  question_id: 0,
+};
