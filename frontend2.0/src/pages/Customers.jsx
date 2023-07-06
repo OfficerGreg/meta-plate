e7227699e7379e9c267f85d260480e182237f131
Stoekli <perak.ivan.1@gmail.com>
2023-07-06 11:31:57 +0200

    added calender

--- frontend2.0/src/pages/Customers.jsx
+++ frontend2.0/src/pages/Customers.jsx
@@ -3,6 +3,10 @@ import React from 'react'
 import Table from '../components/table/Table'
 
 import customerList from '../assets/JsonData/customers-list.json'
+import { $CombinedState } from 'redux'
+import ICalendarLink from "react-icalendar-link";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
 const customerTableHead = [
     'date',
@@ -28,13 +32,29 @@ const renderBody = (item, index) => (
 )
 
 const Customers = () => {
+    const event = {
+        title: "My Title",
+        description: "My Description",
+        startTime: "2018-10-07T10:30:00+10:00",
+        endTime: "2018-10-07T12:00:00+10:00",
+        location: "10 Carlotta St, Artarmon NSW 2064, Australia",
+        attendees: [
+          "Hello World <hello@world.com>",
+          "Hey <hey@test.com>",
+        ]
+      }
+ 
+    
     return (
         <div>
+           
+             
             <h2 className="page-header">
                 calender
             </h2>
             <div className="row">
-                <div className="col-12">
+            <div className="col-12"> <Calendar className="  card " /></div>
+                <div className="col-6">
                     <div className="card">
                         <div className="card__body">
                             <Table
